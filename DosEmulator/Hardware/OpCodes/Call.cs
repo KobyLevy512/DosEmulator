1169332a0b2ca390d1695dc3ba9acc0845099054
Koby Levy <86796339+KobyLevy512@users.noreply.github.com>
2024-03-18 21:49:37 +0200

    Creation of the call instruction

---
+++ DosEmulator/Hardware/OpCodes/Call.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace DosEmulator.Hardware.OpCodes
+{
+    public class Call : Opcode
+    {
+        public override void MapTo(Dictionary<byte, MakeOpcode> map)
+        {
+            map.Add(154, (memory, cpu, reader) =>
+            {
+                //push the code segment to the top of the stack
+                memory.SetDw(cpu.Sp, cpu.Cs);
+                cpu.Sp -= 2;
+
+                //push the current instruction pointer to the top of the stack
+                memory.SetDw(cpu.Sp, cpu.Ip);
+                cpu.Sp -= 2;
+
+                cpu.Cs = reader.ReadUInt16();
+                cpu.Ip = (ushort)(cpu.Ip + reader.ReadInt16());
+            });
+            map.Add(232, (memory, cpu, reader) =>
+            {
+                //push the current instruction pointer to the top of the stack
+                memory.SetDw(cpu.Sp, cpu.Ip);
+                cpu.Sp -= 2;
+
+                //set the instruction pointer to jump to the call
+                cpu.Ip = (ushort)(cpu.Ip + reader.ReadInt16());
+            });
+        }
+    }
+}
