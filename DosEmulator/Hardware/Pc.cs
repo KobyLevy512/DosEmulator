112e82034776b32489f9dd5ea7d738dc5b755219
Koby Levy <86796339+KobyLevy512@users.noreply.github.com>
2024-03-20 08:01:52 +0200

    emprrty

--- DosEmulator/Hardware/Pc.cs
+++ DosEmulator/Hardware/Pc.cs
@@ -7,17 +7,17 @@ namespace DosEmulator.Hardware
     public delegate void MakeOpcode(Memory memory, Cpu cpu, BinaryReader reader);
     public class Pc
     {
-        const int last_db = 128;
-        const int last_dw = 32768;
         //reference to the pc memory
         Memory memory;
+
         //reference to the pc cpu
         Cpu cpu;
-        //reference to the reader
-        BinaryReader reader;
-        //map opcodes to c# function
+
+        //wrapper map for opcodes to appropiate  c# function
         Dictionary<byte, MakeOpcode> map = new Dictionary<byte, MakeOpcode>();
 
+        public Pc():this(new Memory(), new Cpu()) { }
+        public Pc(Memory memory):this(memory, new Cpu()) { }
         public Pc(Memory memory, Cpu cpu)
         {
             this.memory = memory;
